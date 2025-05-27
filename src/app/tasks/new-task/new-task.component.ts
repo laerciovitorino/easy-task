87ac24529bf8cacfb22ce7478752187665d65a01
Laércio Vitorino <laerciovitorino@gmail.com>
2025-05-26 21:06:47 -0300

    Add a way to close(cancel) the modal form and add to it two-way data binding

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-task',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.css'
 })
 export class NewTaskComponent {
-
+  @Output() cancel = new EventEmitter<void>();
+  enteredTitle = '';
+  enteredSummary = '';
+  enteredDate = '';
+  
+  onCancel() {
+    this.cancel.emit();
+  }
 }
